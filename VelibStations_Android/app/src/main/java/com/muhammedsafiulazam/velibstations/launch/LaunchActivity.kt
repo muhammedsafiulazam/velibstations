f49827b709b98d39bff88bc11dd7ea1878a00f54
Muhammed Safiul Azam <muhammed.safiul.azam@gmail.com>
2019-08-23 12:19:18 +0200

    Compiling.

--- VelibStations_Android/app/src/main/java/com/muhammedsafiulazam/velibstations/launch/LaunchActivity.kt
+++ VelibStations_Android/app/src/main/java/com/muhammedsafiulazam/velibstations/launch/LaunchActivity.kt
@@ -1,22 +1,44 @@
 package com.muhammedsafiulazam.velibstations.launch
 
 import android.os.Bundle
+import android.widget.Toast
+import com.muhammedsafiulazam.mobile.WeatherApi
 import com.muhammedsafiulazam.mobile.getPlatformName
 import com.muhammedsafiulazam.velibstations.R
 import com.muhammedsafiulazam.velibstations.activity.BaseActivity
+import io.ktor.client.engine.okhttp.OkHttpConfig
+import io.ktor.client.engine.okhttp.OkHttpEngine
 import kotlinx.android.synthetic.main.activity_launch.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by Muhammed Safiul Azam on 24/07/2019.
  */
 
-class LaunchActivity : BaseActivity() {
+class LaunchActivity : BaseActivity(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
 
         // Test expect / actual.
         lunch_txv_message.text = getPlatformName()
+
+        val weatherApi = WeatherApi(OkHttpEngine(OkHttpConfig()))
+        launch(Dispatchers.Main) {
+            try {
+                val result = withContext(Dispatchers.IO) { weatherApi.fetchWeather() }
+                Toast.makeText(this@LaunchActivity, result.toString(), Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+                Toast.makeText(this@LaunchActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onStart() {
