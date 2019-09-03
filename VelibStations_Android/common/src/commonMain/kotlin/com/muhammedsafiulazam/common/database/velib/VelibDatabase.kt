7cf713f97dfaf74530322a6b3cfc35420e4a9515
Muhammed Safiul Azam <muhammed.safiul.azam@gmail.com>
2019-09-03 17:52:07 +0200

    Minor improvements on database queries.

--- VelibStations_Android/common/src/commonMain/kotlin/com/muhammedsafiulazam/common/database/velib/VelibDatabase.kt
+++ VelibStations_Android/common/src/commonMain/kotlin/com/muhammedsafiulazam/common/database/velib/VelibDatabase.kt
@@ -1,22 +1,33 @@
 package com.muhammedsafiulazam.common.database.velib
 
+import com.muhammedsafiulazam.common.database.velib.schema.Fields
 import com.muhammedsafiulazam.common.service.velib.model.Record
+import com.squareup.sqldelight.Query
 
 
 class VelibDatabase(val db: VelibDB) : IVelibDatabase {
     override fun selectAllRecords() {
-
+        val recordQueries: Query<com.muhammedsafiulazam.common.database.velib.schema.Record> = db.recordQueries.selectAll()
+        recordQueries.executeAsList().forEach { recordQuery ->
+            val fieldsQueries: Query<Fields> = db.fieldsQueries.selectByRecord(recordQuery.id)
+            fieldsQueries.executeAsList().forEach { fieldQuery ->
+                // Initialize models here.
+            }
+        }
     }
 
     override fun insertRecord(record: Record) {
-
+        // Insert fields here.
+        db.recordQueries.insert(record.id!!, record.timestamp)
     }
 
     override fun deleteRecord(record: Record) {
-
+        db.recordQueries.delete(record.id!!)
+        db.fieldsQueries.deleteByRecord(record.id)
     }
 
     override fun deleteAllRecords() {
-
+        db.recordQueries.deleteAll()
+        db.fieldsQueries.deleteAll()
     }
 }
